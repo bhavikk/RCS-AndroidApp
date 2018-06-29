3d46b260a035d5612d80118d8f10faf2c44216b0
Bhavikk <bhavikk.shah@somaiya.edu>
2018-06-29 15:51:50 +0530

    Add clientinfo activity

---
+++ app/src/main/java/com/royalcareerservices/rcs/ClientInfo.java
@@ -0,0 +1,13 @@
+package com.royalcareerservices.rcs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_info);
+    }
+}
